d59255f298ad4c1ea8927297d0e3a79b0af91a5b
Guest User <Guest@epicodus-26.local>
2018-07-11 09:13:56 -0700

    Add form for adding new animals to the database, as well as a way to delete all animals from the database.

--- Animal-Shelter-DB/Controllers/AnimalsController.cs
+++ Animal-Shelter-DB/Controllers/AnimalsController.cs
@@ -12,12 +12,36 @@ namespace AnimalShelter.Controllers
       return View();
     }
 
+    [HttpGet("/Animals/CreateForm")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
     [HttpGet("/Animals/List")]
     public ActionResult List()
     {
       List<Animal> allAnimals = Animal.GetAllAnimals();
 
       return View(allAnimals);
+    }
+
+    [HttpPost("/Animals/List")]
+    public ActionResult AddAnimal(string newtype, string newname, string newgender, int newage)
+    {
+      Animal newAnimal = new Animal(newtype, newname, newgender, newage);
+      newAnimal.SaveAnimal();
+
+      List<Animal> allAnimals = Animal.GetAllAnimals();
+      return View("List", allAnimals);
+    }
+
+    [HttpGet("/Animals/Deleted")]
+    public ActionResult ClearAnimals()
+    {
+      Animal.DeleteAllAnimals();
+      List<Animal> allAnimals = Animal.GetAllAnimals();
+      return View("List", allAnimals);
     }
   }
 }
